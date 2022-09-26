f71593dc4a47f27063a09a1af9998ef7bbc21c52
jmoira <jen.mcquerry98@gmail.com>
2022-09-26 17:04:39 -0400

    Add project files.

---
+++ TestGardenUniversity/Data/GardenSchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using TestGardenUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+namespace TestGardenUniversity.Data
+{
+    public class GardenSchoolContext : DbContext
+    {
+        public GardenSchoolContext(DbContextOptions<GardenSchoolContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Student { get; set; }
+
+        //override in this tutorial to manually rename table property names, not necessary as EF uses Collection names
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
