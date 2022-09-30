ae0a97ad6caea12315afda92ba0a0459e94aaa46
jmoira <jen.mcquerry98@gmail.com>
2022-09-30 09:47:42 -0400

    create about page

--- TestGardenUniversity/Controllers/HomeController.cs
+++ TestGardenUniversity/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using TestGardenUniversity.Models;
+using TestGardenUniversity.Data;
+using TestGardenUniversity.Models.SchoolViewModels;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace TestGardenUniversity.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly GardenSchoolContext _context;
+        public HomeController(ILogger<HomeController> logger, GardenSchoolContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -21,6 +26,18 @@ namespace TestGardenUniversity.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data =
+                from student in _context.Student
+                group student by student.EnrollmentDate into dateGroup
+                select new EnrollmentDateGroup()
+                {
+                    EnrollmentDate = dateGroup.Key,
+                    StudentCount = dateGroup.Count()
+                };
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
